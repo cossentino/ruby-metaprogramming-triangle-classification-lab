3d1827b03bfb2641ace405e8170f795390d2de77
Ian Cossentino <icossentino@gmail.com>
2020-11-25 11:47:44 -0500

    commit tree fac5a35bc3e9a1c92006b3337f535ba0e51e68c7

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,56 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :l1, :l2, :l3, :sides, :sides_arrays
+  
+
+  def initialize(l1,l2,l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+    @sides_arrays = [[@l1, @l2, @l3], [@l2, @l3, @l1], [@l3, @l2, @l1], [@l3, @l1, @l2]]
+    @sides = [@l1, @l2, @l3]
+  end
+
+
+
+  def valid?
+    flag = true
+    [@l1, @l2, @l3].each {|side| side > 0 ? next : flag = false}
+    @sides_arrays.each do |side_array|
+      if side_array[0] + side_array[1] <= side_array[2]
+        flag = false
+      end
+    end
+    flag
+  end
+
+
+  def kind
+    if !valid?
+      raise TriangleError
+    else
+      if @l1 == @l2 && @l2 == @l3
+        :equilateral
+      elsif (@l1 == @l2 && @l1 != @l3) || (@l2 == @l3 && @l1 != @l3) || (@l1 == @l3 && @l2 != @l3)
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+
+  class TriangleError < StandardError
+  end
+
+    
+
+
 end
+
+
+bob = Triangle.new(2,2,2)
+
+#binding.pry
